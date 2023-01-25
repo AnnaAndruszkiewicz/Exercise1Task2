f3bfce5e2403ecbfae73dfc90ad3dc93c978fa11
Anna Andruszkiewicz <119431902+AnnaAndruszkiewicz@users.noreply.github.com>
2023-01-25 15:33:16 +0100

    Add files via upload

---
+++ Exercise1Task1/Exercise1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise1
+{
+    public partial class Form1 : Form
+    {
+        int label1Count = 0;
+        int label2Count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Button 1 pressed " + ++label1Count + " times";
+            label3.Text = "Button1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Button 2 pressed " + ++label2Count + " times";
+            label3.Text = "Button2";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
